6b39e911307907342457b8aa6740ba2edb939601
Frank Jensen <frank.jensen@ucdavis.edu>
2019-10-04 17:39:27 -0500

    add some 2018 triggers

--- analyzers/src/TriggerProducer.cc
+++ analyzers/src/TriggerProducer.cc
@@ -29,6 +29,14 @@ class TriggerProducer : public edm::stream::EDFilter<> {
       int HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTauHPS30_eta2p1_CrossL1_v;
       int HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1_v;
       int HLT_Photon200_v;
+      int HLT_IsoMu24_v;
+      int HLT_IsoTkMu24_v;
+      int HLT_Mu50_v;
+      int HLT_TkMu50_v;
+      int HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v;
+      int HLT_Ele32_WPTight_Gsf_v;
+      int HLT_Ele115_CaloIdVT_GsfTrkIdT_v;
+      int HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v;
       //2017
       int HLT_Photon175_v;
       TTree * tree;
@@ -57,6 +65,19 @@ TriggerProducer::TriggerProducer(const edm::ParameterSet& iConfig)
       "HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1_v/I"
    );
    tree->Branch("HLT_Photon200_v", &HLT_Photon200_v, "HLT_Photon200_v/I");
+   tree->Branch("HLT_IsoMu24_v", &HLT_IsoMu24_v, "HLT_IsoMu24_v/I");
+   tree->Branch("HLT_IsoTkMu24_v", &HLT_IsoTkMu24_v, "HLT_IsoTkMu24_v/I");
+   tree->Branch("HLT_Mu50_v", &HLT_Mu50_v, "HLT_Mu50_v/I");
+   tree->Branch("HLT_TkMu50_v", &HLT_TkMu50_v, "HLT_TkMu50_v/I");
+   tree->Branch(
+      "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v",
+      &HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v,
+      "HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v/I"
+   );
+   tree->Branch("HLT_Ele32_WPTight_Gsf_v", &HLT_Ele32_WPTight_Gsf_v, "HLT_Ele32_WPTight_Gsf_v/I");
+   tree->Branch("HLT_Ele115_CaloIdVT_GsfTrkIdT_v", &HLT_Ele115_CaloIdVT_GsfTrkIdT_v, "HLT_Ele115_CaloIdVT_GsfTrkIdT_v/I");
+   tree->Branch("HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v", &HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v, "HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v/I");
+
    //2017
    tree->Branch("HLT_Photon175_v", &HLT_Photon175_v, "HLT_Photon175_v/I");
 }
@@ -75,11 +96,15 @@ bool TriggerProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
    HLT_Ele24_eta2p1_WPTight_Gsf_LooseChargedIsoPFTauHPS30_eta2p1_CrossL1_v = -1;
    HLT_IsoMu20_eta2p1_LooseChargedIsoPFTauHPS27_eta2p1_CrossL1_v = -1;
    HLT_Photon200_v = -1;
+   HLT_IsoMu24_v = HLT_IsoTkMu24_v = HLT_Mu50_v = HLT_TkMu50_v = -1;
+   HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v = -1;
+   HLT_Ele32_WPTight_Gsf_v = HLT_Ele115_CaloIdVT_GsfTrkIdT_v = HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v = -1;
    //2017
    HLT_Photon175_v = -1;
 
    for (unsigned int j = 0, n = triggerBits->size(); j < n; ++j) {
       std::string trig2 = names.triggerName(j);
+      //std::cout << "trigger " << j << ": " << trig2 << std::endl;
       //2018
       if (trig2.find("HLT_DoubleMediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg_v")!=std::string::npos) {
                       HLT_DoubleMediumChargedIsoPFTauHPS35_Trk1_eta2p1_Reg_v = triggerBits->accept(j);
@@ -92,6 +117,30 @@ bool TriggerProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
       }
       if (trig2.find("HLT_Photon200_v")!=std::string::npos) {
                       HLT_Photon200_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_IsoMu24_v")!=std::string::npos) {
+                      HLT_IsoMu24_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_IsoTkMu24_v")!=std::string::npos) {
+                      HLT_IsoTkMu24_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_Mu50_v")!=std::string::npos) {
+                      HLT_Mu50_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_TkMu50_v")!=std::string::npos) {
+                      HLT_TkMu50_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v")!=std::string::npos) {
+                      HLT_MediumChargedIsoPFTau180HighPtRelaxedIso_Trk50_eta2p1_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_Ele32_WPTight_Gsf_v")!=std::string::npos) {
+                      HLT_Ele32_WPTight_Gsf_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_Ele115_CaloIdVT_GsfTrkIdT_v")!=std::string::npos) {
+                      HLT_Ele115_CaloIdVT_GsfTrkIdT_v = triggerBits->accept(j);
+      }
+      if (trig2.find("HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v")!=std::string::npos) {
+                      HLT_Ele50_CaloIdVT_GsfTrkIdT_PFJet165_v = triggerBits->accept(j);
       }
       //2017
       if (trig2.find("HLT_Photon175_v")!=std::string::npos) {
